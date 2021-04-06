7c9b99187b9e7b63352bbc7f92936113ce04f221
Usama Irfan <usmaa.irfan9797@gmail.com>
2021-04-06 17:22:26 +0500

    Added Carousels

--- js/main.js
+++ js/main.js
@@ -10,3 +10,53 @@ navLinks.forEach(link => {
         document.body.classList.remove('nav-open');
     })
 })
+
+// window.addEventListener('load', function () {
+//     var el = document.querySelectorAll('img.about-me--img');
+//     Lightense(el, {
+//         time: 300,
+//         padding: 40,
+//         offset: 40,
+//         keyboard: true,
+//         cubicBezier: 'cubic-bezier(.2, 0, .1, 1)',
+//         background: 'rgba(255, 255, 255, .98)',
+//         zIndex: 2147483647
+//     });
+// }, false);
+
+$('.carousel').slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    autoplay: true,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 3,
+                infinite: true,
+                dots: true
+            }
+        },
+        {
+            breakpoint: 600,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 2
+            }
+        },
+        {
+            breakpoint: 480,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        }
+        // You can unslick at a given breakpoint now by adding:
+        // settings: "unslick"
+        // instead of a settings object
+    ]
+});
